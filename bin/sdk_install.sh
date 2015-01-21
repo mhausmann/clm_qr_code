113e5bc726a880a57d26204171c7e89a39a17957
Matt Hausmann <github@u6.co.za>
2015-01-21 12:13:10 +0000

    Tweaks to rhn config and SDK install
    Automatic RHN registration
    Better handling of the SDK installation

--- bin/sdk_install.sh
+++ bin/sdk_install.sh
@@ -1,11 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "You need to supply RedHat Network credentials"
+    echo "USAGE: sh setup.sh rhn_username@domain.com rhn_password"
+    exit 0
+fi
 
 SEP="==================================================================="
+ 
+echo $SEP
+echo "Registering with the RedHat Network"
+rhnreg_ks --force --novirtinfo --nopackages --nohardware --username=$1 --password=$2
+echo $SEP
 
 echo $SEP
 echo "Installing pre-requisites"
-yum -y install keyutils-libs-devel libcom_err-devel libselinux-devel libsepol-devel krb5-devel openssl-devel
+yum -y install keyutils-libs-devel libcom_err-devel libselinux-devel libsepol-devel krb5-devel openssl-devel httpd php
 echo $SEP
+ 
+echo "Starting Apache"
+
+chkconfig httpd on
+service httpd start
 
 echo $SEP
 echo "Building and installing Python 2.7"
@@ -25,19 +41,27 @@ echo "Installing Python setup and PIP utilities."
 wget --no-check-certificate https://bootstrap.pypa.io/ez_setup.py -O - | python
 wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py -O - | python
 
-cp `find / -name python-sdk-1.0-src.zip` .
+
+echo $SEP
+echo "Installing CLM SDK"
+cd /opt/bmc
+
+# Uncomment the next line to search for the sdk archive and copy it to the current working directory before continuing.
+# cp `find / -name python-sdk-1.0-src.zip` .
 
 unzip python-sdk-1.0-src.zip
 python setup.py install
 echo $SEP
 
 echo $SEP
-echo "Testing for BMC CLM SDK..."
+echo "Testing for BMC CLM SDK Python package..."
 pip list | grep bmc
 echo $SEP
-cd ~/
+# cd ~/
 echo $SEP
 echo "Done!"
-echo "Now type "clm" to test the SDK"
-echo "Make sure CLM is running before using the SDK!"
+# echo "Now type "clm" to test the SDK"
+# echo "Make sure the CLM services are running before using the SDK!"
+echo "Testing the SDK as $USER. This creates the csmsdk profile directory under $HOME."
+clm -v
 echo $SEP
